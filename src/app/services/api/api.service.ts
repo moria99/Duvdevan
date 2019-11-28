b5793cd1b45aaaefc8c0b8545c0fe9e761ee27c1
chavad <chava7993@gmail.com>
2019-11-28 13:34:48 +0200

    added to api our price details

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  priceDetails() {
+    return this.http.get("http://localhost:3000/priceDetails");
+  }
+  totalPrice() {
+    return this.http.get("http://localhost:3000/totalPrice");
+  }
 }

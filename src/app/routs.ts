27e76ec7584d272ea7c2dd5df0ffa8a1a20200ef
chavad <chava7993@gmail.com>
2019-12-10 11:36:14 +0200

    1

--- src/app/routs.ts
+++ src/app/routs.ts
@@ -1,29 +1,40 @@
-   import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { OrderComponent } from './components/order/order.component';
 import { OrderDetailsComponent } from './components/order/order-details/order-details.component';
+import { PriceDetailsComponent } from './components/order/price-details/price-details.component';
 
-   export const routesConfiguration: Routes = [
+export const routesConfiguration: Routes = [
     {
         path: '',
         children: [
+
             {
                 path: 'log-in',
-                component:LogInComponent,
+                component: LogInComponent,
+            },
+
+            {
+                path: 'log-in/:code',
+                component: LogInComponent,
             },
             {
                 path: 'order',
-                component:OrderComponent,
+                component: OrderComponent,
                 children: [
                     {
                         path: 'newOrder',
-                        component:OrderDetailsComponent,
+                        component: OrderDetailsComponent,
                     },
                     {
-                        path: ':id',
-                        component:OrderDetailsComponent,
+                        path: 'price-details/:c',
+                        component: PriceDetailsComponent,
+                    },
+                    {
+                        path: 'order-details',
+                        component: PriceDetailsComponent,
                     }
                 ]
             }
